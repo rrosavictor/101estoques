f079b477653e218c7d6500acbd62b41e2a8e0671
TiToso00 <rique0022@hotmail.com>
2025-10-26 18:23:19 -0300

    micro front end

---
+++ microfrontend-101estoques/src/pages/Alugueis.jsx
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material'
+
+function Alugueis() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Gerenciamento de Aluguéis
+      </Typography>
+      <Typography variant="subtitle1">
+        Página em construção...
+      </Typography>
+    </div>
+  )
+}
+
+export default Alugueis
